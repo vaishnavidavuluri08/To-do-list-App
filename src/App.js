71f8baeec03663923b0c44f8db91330ff47caf13
Vaishnavi Davuluri <vaishnavidavuluri08@gmail.com>
2024-02-29 17:24:13 -0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+/*import TodoForm from './components/TodoForm';*/
+import TodoList from './components/TodoList';
+import './App.css';
+
+function App() {
+  return (
+    <div className='todo-app'>
+      <TodoList />
+    </div>
+  );
+} 
+
+export default App;
